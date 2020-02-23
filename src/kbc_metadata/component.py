7bf64a34d7a32688429969bf498e304dba1b4efd
Samuel Kozuch <samuel.kozuch@keboola.com>
2020-02-23 17:50:40 +0100

    STYLE: Changed version

--- src/kbc_metadata/component.py
+++ src/kbc_metadata/component.py
@@ -6,7 +6,7 @@ from kbc.env_handler import KBCEnvHandler
 from kbc_metadata.client import MetadataClient, StorageClient
 from kbc_metadata.result import MetadataWriter
 
-APP_VERSION = '0.0.7'
+APP_VERSION = '0.0.8'
 TOKEN_SUFFIX = '_Telemetry_token'
 TOKEN_EXPIRATION_CUSHION = 30 * 60  # 30 minutes
 
